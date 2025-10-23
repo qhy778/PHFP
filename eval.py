006e75bb316e2008508571beb704318625d408e5
qhy778 <quhaoyu7077@163.com>
2025-10-23 16:28:49 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -2,134 +2,113 @@ import os
 import sys
 import argparse
 
-sys.path.append("./")
+sys.path.append("/opt/data/private/PHFP/")
 
-import cv2
-import torch
-import numpy as np
+from tqdm import tqdm
+from multiprocessing import Pool
 
-from pysot.core.config import cfg
-from pysot.models.model_builder import ModelBuilder
-from pysot.tracker.phfp_tracker import PHFP
-from pysot.utils.bbox import get_axis_aligned_bbox
-from pysot.utils.model_load import load_pretrain
-from toolkit.datasets import DatasetFactory
+from tools.draw_success_precision import draw_success_precision
+from toolkit.evaluation import OPEBenchmark
+from toolkit.datasets import *
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+parser = argparse.ArgumentParser(description="Single Object Tracking Evaluation")
+parser.add_argument(
+    "--datasetpath",
+    default="/opt/data/private/test_datasets/",
+    type=str,
+    help="dataset root directory",
+)
+parser.add_argument("--dataset", default="UAV20L", type=str, help="dataset name")
+parser.add_argument(
+    "--tracker_result_dir",
+    default="./results",
+    type=str,
+    help="tracker result root",
+)
+parser.add_argument("--tracker_path", default="/opt/data/private/PHFP/results/", type=str)
+parser.add_argument("--vis", default="True", dest="vis", action="store_true")
+parser.add_argument(
+    "--show_video_level", default=True, dest="show_video_level", action="store_true"
+)
+parser.add_argument("--num", default=1, type=int, help="number of processes to eval")
+args = parser.parse_args()
 
 
 def main():
-    parser = argparse.ArgumentParser(description="-PHFP tracking")
-    parser.add_argument("--dataset", default="UAVTrack112", type=str, help="datasets"),
-    parser.add_argument(
-        "--dataset-root", default="./dataset", type=str, help="dataset root"
+    tracker_dir = os.path.join(args.tracker_path, args.dataset)
+    trackers = os.listdir(tracker_dir)
+
+    root = args.datasetpath + args.dataset
+
+    assert len(trackers) > 0
+    args.num = min(args.num, len(trackers))
+
+    if "UAV123_10fps" in args.dataset:
+        dataset = UAV10Dataset(args.dataset, root)
+    elif "UAV20L" in args.dataset:
+        dataset = UAV20Dataset(args.dataset, root)
+    elif "UAV123" in args.dataset:
+        dataset = UAVDataset(args.dataset, root)
+    elif "DTB70" in args.dataset:
+        dataset = DTBDataset(args.dataset, root)
+    elif "UAVDT" in args.dataset:
+        dataset = UAVDTDataset(args.dataset, root)
+    elif "VISDRONED" in args.dataset:
+        dataset = VISDRONEDDataset(args.dataset, root)
+    elif "UAVTrack112_L" in args.dataset:
+        dataset = UAV112LDataset(args.dataset, root)
+    elif "UAVTrack112" in args.dataset:
+        dataset = UAV112Dataset(args.dataset, root)
+
+    dataset.set_tracker(tracker_dir, trackers)
+    benchmark = OPEBenchmark(dataset)
+    success_ret = {}
+    with Pool(processes=args.num) as pool:
+        for ret in tqdm(
+            pool.imap_unordered(benchmark.eval_success, trackers),
+            desc="eval success",
+            total=len(trackers),
+            ncols=18,
+        ):
+            success_ret.update(ret)
+
+    norm_precision_ret = {}
+    with Pool(processes=args.num) as pool:
+        for ret in tqdm(
+            pool.imap_unordered(benchmark.eval_norm_precision, trackers),
+            desc="eval norm precision",
+            total=len(trackers),
+            ncols=25,
+        ):
+            norm_precision_ret.update(ret)
+
+    precision_ret = {}
+    with Pool(processes=args.num) as pool:
+        for ret in tqdm(
+            pool.imap_unordered(benchmark.eval_precision, trackers),
+            desc="eval precision",
+            total=len(trackers),
+            ncols=20,
+        ):
+            precision_ret.update(ret)
+
+    benchmark.show_result(
+        success_ret,
+        precision_ret,
+        norm_precision_ret,
+        show_video_level=args.show_video_level,
     )
-    parser.add_argument(
-        "--config", default="./experiments/config.yaml", type=str, help="config file"
-    )
-    parser.add_argument(
-        "--snapshot",
-        default="./snapshot/best.pth",
-        type=str,
-        help="snapshot of models to eval",
-    )
-    parser.add_argument("--video", default="", type=str, help="eval one special video")
-    parser.add_argument(
-        "--vis", default="", action="store_true", help="whether visualzie result"
-    )
-    args = parser.parse_args()
 
-    torch.set_num_threads(1)
-
-    cfg.merge_from_file(args.config)
-
-    dataset_root = os.path.join(args.dataset_root, args.dataset)
-    model = ModelBuilder()
-    model = load_pretrain(model, args.snapshot).cuda().eval()
-
-    tracker = PHFP(model)
-
-    dataset = DatasetFactory.create_dataset(
-        name=args.dataset, dataset_root=dataset_root, load_img=False
-    )
-
-    model_name = args.snapshot.split("/")[-1].split(".")[0]
-
-    for v_idx, video in enumerate(dataset):
-        if args.video != "":
-            if video.name != args.video:
-                continue
-
-        toc = 0
-        pred_bboxes = []
-        scores = []
-        track_times = []
-        for idx, (img, gt_bbox) in enumerate(video):
-            tic = cv2.getTickCount()
-            if idx == 0:
-                cx, cy, w, h = get_axis_aligned_bbox(np.array(gt_bbox))
-                gt_bbox_ = [cx - (w - 1) / 2, cy - (h - 1) / 2, w, h]
-                tracker.init(img, gt_bbox_)
-                pred_bbox = gt_bbox_
-                scores.append(None)
-                if "VOT2018-LT" == args.dataset:
-                    pred_bboxes.append([1])
-                else:
-                    pred_bboxes.append(pred_bbox)
-            else:
-                outputs = tracker.track(img)
-                pred_bbox = outputs["bbox"]
-                pred_bboxes.append(pred_bbox)
-                scores.append(outputs["best_score"])
-            toc += cv2.getTickCount() - tic
-            track_times.append((cv2.getTickCount() - tic) / cv2.getTickFrequency())
-            if idx == 0:
-                cv2.destroyAllWindows()
-            if args.vis:
-                try:
-                    gt_bbox = list(map(int, gt_bbox))
-                except:
-                    gt_bbox = [0, 0, 0, 0]
-                    pred_bbox = list(map(int, pred_bbox))
-                    cv2.rectangle(
-                        img,
-                        (gt_bbox[0], gt_bbox[1]),
-                        (gt_bbox[0] + gt_bbox[2], gt_bbox[1] + gt_bbox[3]),
-                        (0, 255, 0),
-                        3,
-                    )
-                    cv2.rectangle(
-                        img,
-                        (pred_bbox[0], pred_bbox[1]),
-                        (pred_bbox[0] + pred_bbox[2], pred_bbox[1] + pred_bbox[3]),
-                        (0, 255, 255),
-                        3,
-                    )
-                    cv2.putText(
-                        img,
-                        str(idx),
-                        (40, 40),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        1,
-                        (0, 255, 255),
-                        2,
-                    )
-                    cv2.imshow(video.name, img)
-                    cv2.waitKey(1)
-        toc /= cv2.getTickFrequency()
-
-        model_path = os.path.join("results", args.dataset, model_name)
-        if not os.path.isdir(model_path):
-            os.makedirs(model_path)
-        result_path = os.path.join(model_path, "{}.txt".format(video.name))
-        with open(result_path, "w") as f:
-            for x in pred_bboxes:
-                f.write(",".join([str(i) for i in x]) + "\n")
-        print(
-            "({:3d}) Video: {:12s} Time: {:5.1f}s Speed: {:3.1f}fps".format(
-                v_idx + 1, video.name, toc, idx / toc
+    if args.vis:
+        for attr, videos in dataset.attr.items():
+            draw_success_precision(
+                success_ret,
+                name=dataset.name,
+                videos=videos,
+                attr=attr,
+                precision_ret=precision_ret,
             )
-        )
 
 
 if __name__ == "__main__":
